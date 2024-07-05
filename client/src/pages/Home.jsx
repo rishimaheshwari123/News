e28fda4241cb7d5e5490993ed1c48ff246bdd470
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-07-05 16:36:27 +0530

    ..

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,12 +7,8 @@ import Reels from "../components/core/HomePage/Reels";
 import PollAns from "../components/core/HomePage/PollAns";
 import Category from "../components/core/HomePage/Category";
 import Tab from "../components/core/HomePage/Tab";
-<<<<<<< HEAD
-import AddSlideBar from "../components/comman/AddSlideBar";
-
-=======
 import VIdeos from "../components/home/VIdeos";
->>>>>>> a5e5b48158256f1e6b08d3306ffa1e21ac795c99
+import AddSlideBar from "../components/comman/AddSlideBar"
 const Home = () => {
   return (
     <div>
