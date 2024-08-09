03ba15baccc205ff928e183d3bbac2fa9df8e44c
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-08-09 14:40:30 +0530

    data change

--- client/src/test/Test.jsx
+++ client/src/test/Test.jsx
@@ -4,7 +4,7 @@ import Hls from 'hls.js';
 const VideoPlayer = () => {
   const videoRef = useRef(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const url = "http://feeds.intoday.in/aajtak/api/aajtakhd/master.m3u8"; // Ensure this URL is correct
+  const url = "http://live.indiaaheadlive.com/3.m3u8"; // Ensure this URL is correct
 
   useEffect(() => {
     const video = videoRef.current;
