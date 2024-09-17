33bce558d214ac37ccb10910f7385eca25c53d8f
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-09-17 13:34:26 +0530

    live update

--- client/src/components/home/LiveSection.jsx
+++ client/src/components/home/LiveSection.jsx
@@ -20,14 +20,48 @@ function LiveSection() {
     fetchStreams();
   }, []);
   return (
-    <>
-      <div className=" flex justify-center mt- gap-3">
+    <div className=" w-full">
+      <div className=" flex justify-center mt- gap-3 w-full">
         <p className=" flex gap-1 items-center text-[16px] bg-red-600 animate-blink p-1 rounded-md text-white font-semibold px-3">
           Live <CgMediaLive />
         </p>
       </div>
-      <LiveTv />
-    </>
+      {/* <LiveTv /> */}
+
+
+      <div className=" w-full ">
+      {liveStreams.length === 0 ? (
+        <div className="text-center">
+          Currently No Live Streaming Is Available
+        </div>
+      ) : (
+        <div className=" gap-4">
+          {liveStreams.map((stream) => (
+            <div key={stream._id} className="bg-white p-4 rounded-lg shadow">
+
+       
+              <div className=" flex justify-between"> 
+              <h2 className="text-xl font-semibold">{stream.name}</h2>
+
+
+              </div>
+              <div className="mt-2">
+                {/* Embedded YouTube Video */}
+                <iframe
+               className="w-full min-h-[270px] "
+                  src={`${stream.url}?autoplay=1`}
+                  title={stream.name}
+                  frameBorder="0"
+                  allow="autoplay; encrypted-media "
+                  allowFullScreen 
+                ></iframe>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+    </div>
   );
 }
 
